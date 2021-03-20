f1bc2b14ff4a8f4950857926bab95434a284a2eb
Vijay_Ky <vijayky007@gmail.com>
2021-03-20 16:45:08 +0530

    updated

---
+++ Resources/ThreadSkeleton.java
@@ -0,0 +1,18 @@
+public class Thread implements Runnable 
+{
+	private Runnable runnable;
+	public Thread(Runnable runnable)
+	{
+		this.runnable = runnable;
+	}
+	public void run()
+	{
+		if(runnable != null)
+		{
+			runnable.run();
+		}
+	}
+	...........
+    ...........
+	...........
+}
